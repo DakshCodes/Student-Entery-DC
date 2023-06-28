dcd0430a18c2296a5b88529570ae96cefb391d7b
DakshCodes <honeysingh617522@gmail.com>
2023-06-28 11:51:56 +0530

    End Of The day

--- pages/edit/[id].js
+++ pages/edit/[id].js
@@ -65,43 +65,43 @@ const index = () => {
 
 
     return (
-        <div className='h-screen flex flex-col justify-evenly items-center'>
-            <h1 className='font-bold text-3xl'>Update User</h1>
-            <form class="w-full max-w-sm" >
-                <div class="md:flex md:items-center mb-6">
-                    <div class="md:w-1/3">
-                        <label class="block text-blue-500 -500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+        <div classNameName='h-screen flex flex-col justify-evenly items-center'>
+            <h1 classNameName='font-bold text-3xl'>Update User</h1>
+            <form className="w-full max-w-sm" >
+                <div className="md:flex md:items-center mb-6">
+                    <div className="md:w-1/3">
+                        <label className="block text-blue-500 -500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
                             Full Name
                         </label>
                     </div>
-                    <div class="md:w-2/3">
-                        <input onChange={(e) => setname(e.target.value)} name="name" value={name} class="bg-gray-200  appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 " id="inline-full-name" type="text" />
+                    <div className="md:w-2/3">
+                        <input onChange={(e) => setname(e.target.value)} name="name" value={name} className="bg-gray-200  appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 " id="inline-full-name" type="text" />
                     </div>
                 </div>
-                <div class="md:flex md:items-center mb-6">
-                    <div class="md:w-1/3">
-                        <label class="block  text-blue-500  font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                <div className="md:flex md:items-center mb-6">
+                    <div className="md:w-1/3">
+                        <label className="block  text-blue-500  font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
                             Email
                         </label>
                     </div>
-                    <div class="md:w-2/3">
-                        <input onChange={(e) => setemail(e.target.value)} name="email" value={email} class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 " id="inline-password" type="email" placeholder="*******@gmail.com" />
+                    <div className="md:w-2/3">
+                        <input onChange={(e) => setemail(e.target.value)} name="email" value={email} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 " id="inline-password" type="email" placeholder="*******@gmail.com" />
                     </div>
                 </div>
-                <div class="md:flex md:items-center mb-6">
-                    <div class="md:w-1/3">
-                        <label class="block text-blue-500  font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                <div className="md:flex md:items-center mb-6">
+                    <div className="md:w-1/3">
+                        <label className="block text-blue-500  font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
                             Number
                         </label>
                     </div>
-                    <div class="md:w-2/3">
-                        <input onChange={(e) => setnumber(e.target.value)} name="number" value={number} class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 " id="inline-password" type="text" placeholder="123456789" />
+                    <div className="md:w-2/3">
+                        <input onChange={(e) => setnumber(e.target.value)} name="number" value={number} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 " id="inline-password" type="text" placeholder="123456789" />
                     </div>
                 </div>
-                <div class="md:flex md:items-end">
-                    <div class="md:w-1/3"></div>
-                    <div class="md:w-2/3">
-                        <button onClick={updateHandle} class="shadow  bg-blue-500 hover:bg-blue-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                <div className="md:flex md:items-end">
+                    <div className="md:w-1/3"></div>
+                    <div className="md:w-2/3">
+                        <button onClick={updateHandle} className="shadow  bg-blue-500 hover:bg-blue-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
                             Update User
                         </button>
                     </div>
